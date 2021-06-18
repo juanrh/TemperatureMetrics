8312f2a0dbb850a32a8eccfb235a608b68df812b
Juan Rodriguez Hortala <juan.rodriguez.hortala@gmail.com>
2021-06-18 15:17:14 +0200

    Remove WIP CDK infra

--- infra/temp-metrics-infra/bin/temp-metrics-infra.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { TempMetricsInfraStack } from '../lib/temp-metrics-infra-stack';
-
-const app = new cdk.App();
-new TempMetricsInfraStack(app, 'TempMetricsInfraStack');
