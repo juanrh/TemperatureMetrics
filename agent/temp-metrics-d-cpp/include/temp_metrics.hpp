d24518ee622e14e2b4c88548cb0864c48c680e19
Juan Rodriguez Hortala <juan.rodriguez.hortala@gmail.com>
2021-06-22 21:05:57 +0200

    some interface polish

--- agent/temp-metrics-d-cpp/include/temp_metrics.hpp
+++ agent/temp-metrics-d-cpp/include/temp_metrics.hpp
@@ -6,17 +6,21 @@
 
 struct TempMeasurement {
     std::time_t timestamp;
-    int temp;
+    float temp;
+    float humidity;
 };
 
-template<class Measurement>
+template<typename Measurement>
 class Sensor {
     virtual ~Sensor();
+    /** Only valid measurements should be returned, e.g. discard NaN returned by the driver 
+     * and retry until a valid measurement is obtained*/
     virtual Measurement measure();
 };
 
-template<class Measurement>
+template<typename Measurement>
 class Sink {
+    virtual ~Sink();
     virtual void publish(std::shared_ptr<Measurement> measurement);
 };
 
