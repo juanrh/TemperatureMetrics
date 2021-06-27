515b1211aaf9c5e6d2f38bfdfa861891ff18d59f
Juan Rodriguez Hortala <juan.rodriguez.hortala@gmail.com>
2021-06-27 18:51:51 +0200

    cleanup

--- agent/temp-metrics-d-cpp/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "temp_metrics.hpp"
-
-using namespace std;
-
-int main()
-{
-    cout << "Hello world!" << endl;
-}
--- agent/temp-metrics-d/src/main.rs
+++
@@ -1,9 +0,0 @@
-use std::{thread, time};
-
-fn main() {
-    let measurement_periodicity = time::Duration::from_secs(1);
-    loop {
-        println!("Measuring the temperature!");
-        thread::sleep(measurement_periodicity);   
-    }
-}
