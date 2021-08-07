79ab3e7c52f85a23c7b7c6eaf90ee82d57231e1a
Juan Rodriguez Hortala <juan.rodriguez.hortala@gmail.com>
2021-08-07 19:50:14 +0200

    CMake project setup for online agent

---
+++ agent/online-temp-metrics/README.md
@@ -0,0 +1,10 @@
+# Online temp metrics daemon
+
+## Devel environment
+
+```bash
+mkdir -p build && cd build
+cmake .. -GNinja
+ninja
+./main
+```
---
+++ agent/online-temp-metrics/src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "hello word!\n";
+
+    std::cout << "bye!" << std::endl;
+    return 0;
+}
