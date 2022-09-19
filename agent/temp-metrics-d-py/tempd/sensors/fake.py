1dbb530fd89b0c3413ba0929bc2f6892131bb672
Juan Rodriguez Hortala <juan.rodriguez.hortala@gmail.com>
2022-09-19 19:53:00 +0200

    Fix fake sensor docstring

--- agent/temp-metrics-d-py/tempd/sensors/fake.py
+++ agent/temp-metrics-d-py/tempd/sensors/fake.py
@@ -1,13 +1,5 @@
-"""DHT11 sensor
-Precision is +/- 2 degree temp measurement, it always returns floats
-with decimal part equal to 0.
-The module is only available for supported platforms like Raspberry Pi,
-so a stub `measure()` function is defined for other platforms
-
-References:
-
-- https://wiki.seeedstudio.com/Grove-TemperatureAndHumidity_Sensor/
-- https://raspberrypi.stackexchange.com/questions/47230/how-to-get-the-accurate-temperature-readings-from-dht11-sensor-on-grovepi # pylint: disable=line-too-long
+"""Fake sensor
+Fake sensor to use for development outside of the target platform.
 """
 
 import time
